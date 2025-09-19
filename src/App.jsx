7a79139cb58a4be29c98c4b49e3448a3ddb1c977
Pietro <pietro.digiovanni.dev@gmail.com>
2025-09-19 12:53:39 +0200

    Supabase init + test data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
 function App() {
-    return <h1>Hello World</h1>;
+    const [instruments, setInstruments] = useState([]);
+
+    useEffect(() => {
+        getInstruments();
+    }, []);
+
+    async function getInstruments() {
+        const { data } = await supabase.from("ingredients").select('*');
+        setInstruments(data);
+    }
+
+    return (
+        <ul>
+            {instruments.map((instrument) => (
+                <li key={instrument.name}>{instrument.name}</li>
+            ))}
+        </ul>
+    );
 }
 
 export default App;
