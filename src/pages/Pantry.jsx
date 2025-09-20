902dde59a6375d12dc7d3daa534699b0b8c99b68
Pietro <pietro.digiovanni.dev@gmail.com>
2025-09-20 12:22:45 +0200

    update pantry ui

--- src/pages/Pantry.jsx
+++ src/pages/Pantry.jsx
@@ -1,15 +1,15 @@
 import { useContext } from 'react';
 import { GlobalContext } from '../contexts/GlobalContextProvider.jsx';
 
-import { ListGroup, DropdownButton, Dropdown, InputGroup, Form, Row, Col } from 'react-bootstrap';
+import { ListGroup, DropdownButton, Dropdown, InputGroup, Form, Row, Col, Button } from 'react-bootstrap';
 
 export default function Pantry() {
-    const { instruments } = useContext(GlobalContext);
+    const { filteredIngredients } = useContext(GlobalContext);
 
     return (
         <>
             <Form>
-                <Row>
+                <Row className="align-items-center">
                     <InputGroup className="mb-3">
                         <Col>
                             <Form.Control placeholder="Ingredient name" />
@@ -17,12 +17,23 @@ export default function Pantry() {
                         <Col>
                             <Form.Control placeholder="Ingredient quantity" />
                         </Col>
+                        <Col>
+                            <Form.Select aria-label="Default select example">
+                                <option value="kg">kg</option>
+                                <option value="g">g</option>
+                                <option value="l">l</option>
+                                <option value="ml">ml</option>
+                                <option value="unit">unit</option>
+                            </Form.Select></Col>
+                        <Col>
+                            <Button variant="primary">Add</Button>
+                        </Col>
                     </InputGroup>
                 </Row>
             </Form>
 
             <ListGroup>
-                {instruments.map((el) =>
+                {filteredIngredients.map((el) =>
                     <ListGroup.Item as="li" key={el.id} className="d-flex justify-content-between">
                         <span>{el.name}</span>
                         <span>{`${el.quantity} ${el.unit}`}</span>
